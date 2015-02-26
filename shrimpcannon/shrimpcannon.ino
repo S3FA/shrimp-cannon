446a1018f1548157a43d0e69561f2e41a2ff48f3
Kyle White <kylewhite@gmail.com>
2015-02-25 21:01:03 -0500

    Clean up and add channels

--- shrimpcannon/shrimpcannon.ino
+++ shrimpcannon/shrimpcannon.ino
@@ -4,7 +4,6 @@
 #define TLC_ON 4095
 #define TLC_OFF 0
 
-#define SHRIMP 0
 Button button = Button(14, BUTTON_PULLUP_INTERNAL, true, 1000);
 
 void setup() {
@@ -15,14 +14,19 @@ void setup() {
 
 void loop() {
   if(button.isPressed() ) {
-    Tlc.set(SHRIMP,TLC_ON);
-    Tlc.update();
-    delay(300);
-    Tlc.set(1,TLC_ON);
-    Tlc.update();
-    delay(300);
-    Tlc.set(2,TLC_ON);
-    Tlc.update();
+    fireAndWait(0, 200);
+    fireAndWait(1, 200);
+    fireAndWait(2, 200);
+    fireAndWait(3, 200);
+    fireAndWait(4, 200);
+    fireAndWait(5, 200);
+    fireAndWait(6, 200);
   }
   
+}
+
+void fireAndWait(int channel, int delayMs) {
+    Tlc.set(channel,TLC_ON);
+    Tlc.update();
+    delay(delayMs);  
 }
