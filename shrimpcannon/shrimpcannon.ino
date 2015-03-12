2fc468cb0d638c4907529ca0ed7d001bc13812f1
katee <hello@kate.io>
2015-03-11 21:55:32 -0400

    Updates program to have easy to configure timings.

--- shrimpcannon/shrimpcannon.ino
+++ shrimpcannon/shrimpcannon.ino
@@ -3,42 +3,57 @@
 
 #define TLC_ON 4095
 #define TLC_OFF 0
-#define GAP_TIME 60
+#define BUTTON_PIN 14
+#define NUM_EFFECTS 5
 
-Button button = Button(14, BUTTON_PULLUP_INTERNAL, true, 1000);
-boolean fired;
+Button button = Button(BUTTON_PIN, BUTTON_PULLUP_INTERNAL, true, 100);
+
+// when an effect should start
+int onTimeouts[NUM_EFFECTS] = {0, 60, 120, 180, 0};
+// how long an effect is on for
+int onLength[NUM_EFFECTS] = {10, 60, 60, 60, 200};
+int offTimeouts[NUM_EFFECTS];
 
 void setup() {
+  for (int i = 0; i < NUM_EFFECTS; i++) {
+    offTimeouts[i] = onTimeouts[i] + onLength[i];
+  }
+
   Tlc.init();
   Tlc.clear();
   Tlc.update();
-  fired = false;
+  
+  button.pressHandler(onPress);
 }
 
-void loop() {
-  if(button.isPressed() && !fired) {
-    fired = true;
-    for(int i = 0; i < 5; i++) {
-      fireAndWait(i, GAP_TIME);  
-    }
-  }
-
-  if(fired) {
-    fireOFF(500);
-    if(!button.isPressed()) {
-      fired = false;
-    } 
-  }
-}
-
-void fireAndWait(int channel, int delayMs) {
-  Tlc.set(channel,TLC_ON);
-  Tlc.update();
-  delay(delayMs);  
-}
-
-void fireOFF(int delayMs) {
-  delay(delayMs);
+void loop() { 
+  button.process();
   Tlc.clear();
   Tlc.update();
+}
+
+void onPress(Button& b){
+  doRoutine(millis());
+}
+
+// actually do the routine
+void doRoutine(long startTime) {
+  while (true) {
+    int loopTime = millis() - startTime;
+    
+    for (int i = 0; i < NUM_EFFECTS; i++) {
+      if (loopTime > onTimeouts[i] && loopTime < offTimeouts[i]) {
+        Tlc.set(i, TLC_ON);
+      } else {
+        Tlc.set(i, TLC_OFF);
+      }
+    }
+    
+    Tlc.update();
+    
+    // leave loop if past last routine step
+    if (loopTime > offTimeouts[NUM_EFFECTS - 1]) {
+      break;
+    }
+  }
 }
